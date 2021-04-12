058ae4a63876810e7406d714a8c08927280deb43
Faisal Mustafa <faisalm@microsoft.com>
2021-04-12 17:15:10 -0500

    Add project files.

---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            // Get genres via LINQ
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                        select m;
+            
+            if (!string.IsNullOrEmpty(SearchString))
+            {                
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            // Movie = await _context.Movie.ToListAsync();
+            // The Genre List does not sort. come back to this point to implement sort onclick function, figure out the right code
+            // cshtml and code behind.
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
